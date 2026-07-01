9b83ba57d8c69d2486cb01594cc125b5eb018367
crystal <cheesedrone@gmail.com>
2026-07-01 11:02:06 -0700

    spawning and recollisioning jar

---
+++ scenes/eyes/eye.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+@export var info: EyeInfo 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass#if  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scenes/general/spawner.gd
+++ scenes/general/spawner.gd
@@ -1,10 +1,10 @@
 extends Node2D
-@export var eye_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
+@export var eye_scene: PackedScene
+@onready var spawn_point: Vector2 = $SpawnPoint.global_position
+
 func _ready() -> void:
 	SignalBus.spawn_eye.connect(spawn) # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 func spawn(type: String) -> void:
 	var inst = eye_scene.instantiate()
+	inst.global_position = spawn_point
 	add_child(inst)
