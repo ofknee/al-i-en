3eeed2be4dce4ef7ab7cccd3eebf7b0d4d7f76da
crystal <cheesedrone@gmail.com>
2026-06-30 21:49:33 -0700

    button

--- scenes/general/globals/signal_bus.gd
+++ scenes/general/globals/signal_bus.gd
@@ -2,12 +2,14 @@ extends Node
 
 signal spawn_eye(eye: String)
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+##SignalBus.change_odds.connect(inc_odds)
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+##		SignalBus.change_odds.emit(-(INFO[type]["strength"]))
